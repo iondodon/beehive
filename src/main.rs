a081e111fed907e39c0aca67d419f603bbe88467
Ion Dodon <iondodon2@gmail.com>
2024-02-08 12:08:04 +0200

    Added tokio dependency

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
