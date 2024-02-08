46700d6644b975e368c868b0b26d1ce17ca05ff9
Ion Dodon <iondodon2@gmail.com>
2024-02-08 12:36:16 +0200

    Handle commands

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Accepted connection from {}", addr);
 
         spawn(async move {
-            let mut buf = vec![0; 1024];
+            let mut buf = [0; 1024];
 
             loop {
                 let n = match socket.read(&mut buf).await {
@@ -25,6 +25,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         return;
                     }
                 };
+
+                handle_command(&buf[..n]);
                 
                 if let Err(e) = socket.write_all(&buf[..n]).await {
                     eprintln!("Failed to write to socket; err = {:?}", e);
@@ -32,5 +34,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         });
+    }
+}
+
+fn handle_command(command: &[u8]) {
+    if let Ok(data) = std::str::from_utf8(command) {
+        let data = data.trim_matches(char::from(0)).trim();
+        
+        let parts: Vec<&str> = data.split_whitespace().collect();
+        
+        if parts.len() == 3 {
+            match parts.as_slice() {
+                ["SET", key, value] => {
+                    println!("Command: SET, Key: {}, Value: {}", key, value);
+                },
+                _ => println!("Unknown command or incorrect format"),
+            }
+        } else {
+            println!("Incorrect format");
+        }
+    } else {
+        println!("Data is not valid UTF-8");
     }
 }
